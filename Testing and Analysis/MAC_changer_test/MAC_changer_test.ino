b991a95201b53207844202c5ca6a8e68540fd073
Aswin Raghuprasad <aswinrprasad@gmail.com>
2020-01-29 11:20:37 +0530

    MAC-Spoofing to add security

---
+++ Testing and Analysis/MAC_changer_test/MAC_changer_test.ino
@@ -0,0 +1,95 @@
+#include <DHT.h>  // Including library for dht
+
+#include <ESP8266WiFi.h>
+#include "ChangeMac.hpp"
+ 
+String apiKey = "4VMADMVD8WTB0SLB";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "Bharath";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "qazedcwsx";
+const char* server = "api.thingspeak.com";
+
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+#define DHTPIN 5          //pin where the dht11 is connected
+ 
+DHT dht(DHTPIN, DHT11);
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       dht.begin();
+        Serial.println();
+        Serial.println();
+        
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+        WiFi.softAPConfig(local_IP, gateway, subnet);
+        WiFi.softAP("NODEMCU");
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ 
+}
+ 
+void loop() 
+{
+  
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             postStr +="&field2=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%. Send to Thingspeak.");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  uint8_t mac[6];
+  makeRandomMac(mac);
+  changeMac(mac);
+  Serial.print("MAC address is ");
+  Serial.println(WiFi.macAddress());
+  WiFi.begin(ssid, pass);
+  delay(10000);
+}

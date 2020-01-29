b35b35062eec0657228e7d256760daf873ec2426
Aswin Raghuprasad <aswinrprasad@gmail.com>
2020-01-29 14:33:47 +0530

    MAC Spoof firewall

--- Testing and Analysis/MAC_changer_test/MAC_changer_test.ino
+++ Testing and Analysis/MAC_changer_test/MAC_changer_test.ino
@@ -17,77 +17,71 @@ IPAddress subnet(255,255,255,0);
  
 DHT dht(DHTPIN, DHT11);
 int cnt=0;
-int mx = 10;
+int mx = 5;
 int f=0;
 WiFiClient client;
  
 void setup() 
 {
-       Serial.begin(115200);
-       delay(10);
-       dht.begin();
-        Serial.println();
-        Serial.println();
-        
-       Serial.println("Connecting to ");
-       Serial.println(ssid);
- 
-       WiFi.begin(ssid, pass);
- 
-      while (WiFi.status() != WL_CONNECTED) 
-     {
-            delay(500);
-            Serial.print(".");
-     }
-      Serial.println("");
-      Serial.println("WiFi connected");
- 
+  Serial.begin(115200);
+  delay(10);
+  dht.begin();
+  Serial.println();
+  Serial.println();
+  
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 }
-int getchan(){
-  return random(1+rand()%((11+1)-1));
-}
+
+
 void loop() 
 {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
   
-      float h = dht.readHumidity();
-      float t = dht.readTemperature();
-      
-              if (isnan(h) || isnan(t)) 
-                 {
-                     Serial.println("Failed to read from DHT sensor!");
-                      return;
-                 }
-
-                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
-                      {  
+  if (isnan(h) || isnan(t)) 
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+        
+  if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+  {  
                             
-                             String postStr = apiKey;
-                             postStr +="&field1=";
-                             postStr += String(t);
-                             postStr +="&field2=";
-                             postStr += String(h);
-                             postStr += "\r\n\r\n";
+    String postStr = apiKey;
+    postStr +="&field1=";
+    postStr += String(t);
+    postStr +="&field2=";
+    postStr += String(h);
+    postStr += "\r\n\r\n";
  
-                             client.print("POST /update HTTP/1.1\n");
-                             client.print("Host: api.thingspeak.com\n");
-                             client.print("Connection: close\n");
-                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-                             client.print("Content-Type: application/x-www-form-urlencoded\n");
-                             client.print("Content-Length: ");
-                             client.print(postStr.length());
-                             client.print("\n\n");
-                             client.print(postStr);
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
  
-                             Serial.print("Temperature: ");
-                             Serial.print(t);
-                             Serial.print(" degrees Celcius, Humidity: ");
-                             Serial.print(h);
-                             Serial.println("%. Send to Thingspeak.");
-                        }
-          client.stop();
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" degrees Celcius, Humidity: ");
+    Serial.print(h);
+    Serial.println("%. Send to Thingspeak.");
+  }
+  client.stop();
  
-          Serial.println("Waiting...");
-  //WiFi.disconnect();
+  Serial.println("Waiting...");
   cnt++;
   if(cnt==mx){
     uint8_t mac[6];
